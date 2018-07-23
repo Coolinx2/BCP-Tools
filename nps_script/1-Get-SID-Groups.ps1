afd0d7690fb690e41eb9679093be03fa9fbe36b9
Brandon <brandon-spendlove@hotmail.co.uk>
2018-07-23 19:34:50 +0100

    Add files via upload

---
+++ nps_script/1-Get-SID-Groups.ps1
@@ -0,0 +1,8 @@
+﻿$csvName = "customer_info.csv"
+
+$output = new-object PSObject
+$vlan = "VLAN*"
+$output = Get-ADGroup -filter {name -like $vlan} | Select-Object Name, SID
+$output | Export-Csv $csvName -NoTypeInformation
+
+
